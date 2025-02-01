da8971d1d7e387f011edc82757d11561935819ea
Neha Rana <121093178+neha13rana@users.noreply.github.com>
2025-02-01 12:16:13 +0000

    FAQ model development

--- faq_project/faq_app/models.py
+++ faq_project/faq_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class FAQ(models.Model):
+    question = models.TextField()  
+    answer = RichTextField()  
+    
+    question_hi = models.TextField(blank=True, null=True)  
+    question_bn = models.TextField(blank=True, null=True)  
+    
+    answer_hi = RichTextField(blank=True, null=True)
+    answer_bn = RichTextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.question
+
+    def get_translated_question(self, lang):
+        """Returns the question in the desired language."""
+        if lang == 'hi':
+            return self.question_hi or self.question
+        elif lang == 'bn':
+            return self.question_bn or self.question
+        return self.question 
+    
+    def get_translated_answer(self, lang):
+        """Returns the answer in the desired language."""
+        if lang == 'hi':
+            return self.answer_hi or self.answer
+        elif lang == 'bn':
+            return self.answer_bn or self.answer
+        return self.answer  
